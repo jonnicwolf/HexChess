ce080b78931a0e3102aedb51582a346b7ebf969c
Fezz <jonnicwolf@gmail.com>
2023-10-13 11:42:24 -0400

    fixes linting errors

--- HexChess/src/App.jsx
+++ HexChess/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 function App() {
   const canvasRef = useRef(null);
@@ -46,6 +46,6 @@ function App() {
       ref={canvasRef}
     ></canvas>
   );
-};
+}
 
 export default App;
